f9fa372eefe6a25ff3d270135e4a7ea842c3eda1
Løvgren, Dag Erik <dagerikhl@gmail.com>
2024-12-12 12:48:44 +0100

    Improve styling of player log

--- src/features/player-log/components/PlayerLogView.tsx
+++ src/features/player-log/components/PlayerLogView.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { Code } from "@/common/components/formatting/Code";
 import { Dl } from "@/common/components/layout/Dl";
-import { IPlayerLog } from "@/features/player-log/types/IPlayerLog";
-import { Fragment } from "react";
-import { IPlayer } from "../types/IPlayer";
 import { formatDatetime } from "@/common/utils/formatting/datetime";
+import { IPlayerLog } from "@/features/player-log/types/IPlayerLog";
+import { IPlayer } from "../types/IPlayer";
+import styles from "./PlayerLogView.module.scss";
 
 const getPlayerName = (id: string, players: Record<string, IPlayer>): string =>
   players[id]?.name ?? id;
@@ -19,62 +20,53 @@ export const PlayerLogView = ({ playerLog }: IPlayerLogViewProps) => {
   }
 
   return (
-    <Dl>
-      <dt>Current players:</dt>
-      <dd>
-        {playerLog.currentPlayers.length > 0
-          ? `[${playerLog.currentPlayers.length}]`
-          : "-"}{" "}
-        {playerLog.currentPlayers
-          .map((id) => getPlayerName(id, playerLog.players))
-          .join(", ")}
-      </dd>
+    <div className={styles.container}>
+      <Dl className={styles["dl-container"]}>
+        <dt>Current players:</dt>
+        <dd>
+          {playerLog.currentPlayers.length > 0
+            ? `[${playerLog.currentPlayers.length}]`
+            : "-"}{" "}
+          {playerLog.currentPlayers
+            .map((id) => getPlayerName(id, playerLog.players))
+            .join(", ")}
+        </dd>
 
-      <dt>Player overview:</dt>
-      <dd>
-        {Object.keys(playerLog.players).length > 0 ? (
-          <Dl>
-            {Object.values(playerLog.players).map(
-              ({ id, name, player, character }) => (
-                <Fragment key={id}>
-                  <dt>{id}:</dt>
-                  <dd>
-                    Name={name ?? "?"}, Player={player ?? "?"}, Character=
+        <dt>Player overview:</dt>
+        <dd>
+          {Object.keys(playerLog.players).length > 0
+            ? Object.values(playerLog.players).map(
+                ({ id, name, player, character }) => (
+                  <div key={id}>
+                    {id}: Name={name ?? "?"}, Player={player ?? "?"}, Character=
                     {character ?? "?"}
-                  </dd>
-                </Fragment>
-              ),
-            )}
-          </Dl>
-        ) : (
-          "-"
-        )}
-      </dd>
+                  </div>
+                ),
+              )
+            : "-"}
+        </dd>
 
-      <dt>Log:</dt>
-      <dd>
-        {playerLog.entries.length > 0 ? (
-          <Dl>
-            {playerLog.entries.map(
-              ({ id, event, timestamp, nextCurrentPlayers }) => (
-                <Fragment key={`${id}-${event}-${timestamp}`}>
-                  <dt>{formatDatetime(timestamp, true)}:</dt>
-                  <dd>
-                    {getPlayerName(id, playerLog.players)} {event} [current
-                    players=
-                    {nextCurrentPlayers
-                      .map((id) => getPlayerName(id, playerLog.players))
-                      .join(", ")}
-                    ]
-                  </dd>
-                </Fragment>
-              ),
-            )}
-          </Dl>
-        ) : (
-          "-"
-        )}
-      </dd>
-    </Dl>
+        <dt>Log:</dt>
+        <dd>
+          {playerLog.entries.length > 0
+            ? playerLog.entries.map(
+                ({ id, event, timestamp, nextCurrentPlayers }) => (
+                  <div key={`${id}-${event}-${timestamp}`}>
+                    <Code>
+                      {formatDatetime(timestamp, true)}:{" "}
+                      {event === "logged-on" ? "+" : "-"}
+                      {getPlayerName(id, playerLog.players)} [
+                      {nextCurrentPlayers
+                        .map((id) => getPlayerName(id, playerLog.players))
+                        .join("+")}
+                      ]
+                    </Code>
+                  </div>
+                ),
+              )
+            : "-"}
+        </dd>
+      </Dl>
+    </div>
   );
 };
